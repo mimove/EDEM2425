c7b6a4040bd8ae734273b492709f33a3b1ba16f0
mimove <mimove14@gmail.com>
2024-12-15 15:15:37 +0100

    [ADDED] Adding logic to syncronize orders to analytical db in clickhouse

--- cloud_intro/end2end/analytical_layer/el_database/main.py
+++ cloud_intro/end2end/analytical_layer/el_database/main.py
@@ -1,123 +1,148 @@
+import logging
 import os
 from datetime import datetime
 
 import psycopg2
 from clickhouse_driver import Client
 
-# Database configuration
-OLTP_DB_CONFIG = {
-    "dbname": "ecommerce",
-    "user": "user",
-    "password": "password",
-    "host": os.getenv('HOST_IP'),  # Change to the OLTP DB host
-    "port": 5432,
-}
 
-OLAP_DB_CONFIG = {
-    "host": "localhost",  # Change to the OLAP DB host
-    "port": 8124,
-    "database": "analytics_db",  # Set to your OLAP database name
-    "user": "user",
-    "password": "password",
-    "send_receive_timeout": 120
-}
+class DbManager:
+    def __init__(self, OLTP_DB_CONFIG, OLAP_DB_CONFIG):
+        self.OLTP_DB_CONFIG = OLTP_DB_CONFIG
+        self.OLAP_DB_CONFIG = OLAP_DB_CONFIG
+        self.conn_oltp = None
+        self.conn_olap = None
 
-def sync_data():
-    """Synchronize tables from OLTP to OLAP without downtime."""
-    # try:
-    # Connect to OLTP and OLAP databases
-    conn_oltp = psycopg2.connect(**OLTP_DB_CONFIG)
-    print('Connection to operational_db successful')
-    cursor_oltp = conn_oltp.cursor()
-    client_olap = Client(**OLAP_DB_CONFIG)
-    print('Connection to analytical_db successful')
+    def _connect_postgres(self):
+        try:
+            self.conn_oltp = psycopg2.connect(**self.OLTP_DB_CONFIG)
+            logging.info("Connected to PostgreSQL successfully.")
+            return self.conn_oltp.cursor()
+        except Exception as e:
+            logging.error(f"Failed to connect to PostgreSQL: {e}")
+            raise
 
-    # Replace customers
-    cursor_oltp.execute("SELECT * FROM customers")
-    customers = cursor_oltp.fetchall()
-    client_olap.execute("DROP TABLE IF EXISTS customers_temp")
-    print('droping table')
-    client_olap.execute("""
-        CREATE TABLE customers_temp (
-            id UInt32,
-            customer_name String,
-            email String
-        ) ENGINE = MergeTree() ORDER BY id
-    """)
-    print('customers_temp created')
-    client_olap.execute(
-        "INSERT INTO customers_temp (id, name, email) VALUES", customers
-    )
-    print('customers_temp populated')
-    client_olap.execute("RENAME TABLE customers TO customers_old, customers_temp TO customers")
-    client_olap.execute("DROP TABLE IF EXISTS customers_old")
-    print(f"Synchronized {len(customers)} customers.")
+    def _connect_clickhouse(self):
+        try:
+            self.conn_olap = Client(**self.OLAP_DB_CONFIG)
+            logging.info("Connected to ClickHouse successfully.")
+            return self.conn_olap
+        except Exception as e:
+            logging.error(f"Failed to connect to ClickHouse: {e}")
+            raise
 
-    # Replace products
-    cursor_oltp.execute("SELECT id, name, price FROM products")
-    products = cursor_oltp.fetchall()
-    client_olap.execute("DROP TABLE IF EXISTS products_temp")
-    client_olap.execute("""
-        CREATE TABLE products_temp (
-            id UInt32,
-            product_name String,
-            price Float32
-        ) ENGINE = MergeTree() ORDER BY id
-    """)
-    client_olap.execute(
-        "INSERT INTO products_temp (id, name, price) VALUES", products
-    )
-    client_olap.execute("RENAME TABLE products TO products_old, products_temp TO products")
-    client_olap.execute("DROP TABLE IF EXISTS products_old")
-    print(f"Synchronized {len(products)} products.")
+    def get_operational_data(self, table):
+        try:
+            cursor_oltp = self._connect_postgres()
+            cursor_oltp.execute(f"SELECT * FROM {table}")
+            data = cursor_oltp.fetchall()
+            return data
+        except Exception as e:
+            logging.error(f"Error during customer synchronization: {e}")
+        finally:
+            if cursor_oltp:
+                cursor_oltp.close()
+                logging.info("Closed PostgreSQL connection.")
 
-    # Replace orders
-    cursor_oltp.execute("SELECT id, customer_id, timestamp, total_price FROM orders")
-    orders = cursor_oltp.fetchall()
-    client_olap.execute("DROP TABLE IF EXISTS orders_temp")
-    client_olap.execute("""
-        CREATE TABLE orders_temp (
-            id UInt32,
-            customer_id UInt32,
-            created_at DateTime,
-            total_price Float32
-        ) ENGINE = MergeTree() ORDER BY id
-    """)
-    client_olap.execute(
-        "INSERT INTO orders_temp (id, customer_id, created_at, total_price) VALUES", orders
-    )
-    client_olap.execute("RENAME TABLE orders TO orders_old, orders_temp TO orders")
-    client_olap.execute("DROP TABLE IF EXISTS orders_old")
-    print(f"Synchronized {len(orders)} orders.")
+    def write_to_analytical_db(self, data, table, table_ddl, values):
+        try:
+            client_olap = self._connect_clickhouse()
+            logging.info(f"Fetched {len(data)} {table} from PostgreSQL.")
+            client_olap.execute(f"DROP TABLE IF EXISTS {table}_temp")
+            client_olap.execute(table_ddl)
+            logging.info(f"Created table {table}_temp in ClickHouse.")
+            query = f"INSERT INTO analytics_db.{table}_temp VALUES {values}"
+            client_olap.execute(query)
+            logging.info(f"Inserted data into {table}_temp table.")
+            client_olap.execute(f"""
+                RENAME TABLE {table} TO {table}_old, {table}_temp TO {table}
+            """)
+            client_olap.execute(f"DROP TABLE IF EXISTS {table}_old")
+            logging.info(f"Successfully synchronized {table}  table.")
+        except Exception as e:
+            logging.error(f"Error during customer synchronization: {e}")
 
-    # Replace order_products
-    cursor_oltp.execute("SELECT order_id, product_id, quantity, price FROM order_products")
-    order_products = cursor_oltp.fetchall()
-    client_olap.execute("DROP TABLE IF EXISTS order_products_temp")
-    client_olap.execute("""
-        CREATE TABLE order_products_temp (
-            order_id UInt32,
-            product_id UInt32,
-            quantity UInt32,
-            price Float32
-        ) ENGINE = MergeTree() ORDER BY (order_id, product_id)
-    """)
-    client_olap.execute(
-        "INSERT INTO order_products_temp (order_id, product_id, quantity, price) VALUES",
-        order_products
-    )
-    client_olap.execute("RENAME TABLE order_products TO order_products_old, order_products_temp TO order_products")
-    client_olap.execute("DROP TABLE IF EXISTS order_products_old")
-    print(f"Synchronized {len(order_products)} order-product relationships.")
 
-    print(f"Synchronization completed at {datetime.now()}.")
+def syncronize_customers(db_conn_manager, data):
+    customers_ddl = """CREATE TABLE customers_temp (id UInt32, customer_name String,
+                                                    email String)
+                                                    ENGINE = MergeTree() ORDER BY id"""
+    customer_values = ", ".join(
+                f"({row[0]}, '{row[1]}', '{row[2]}')" for row in data
+            )
+    db_conn_manager.write_to_analytical_db(data, 'customers', customers_ddl,
+                                           customer_values)
 
-    # except Exception as e:
-    #     print(f"Error during synchronization: {e}")
 
-    # finally:
-    #     cursor_oltp.close()
-    #     conn_oltp.close()
+def syncronize_products(db_conn_manager, data):
+    products_ddl = """CREATE TABLE products_temp (id UInt32, product_name String,
+                                                  price Float32) ENGINE = MergeTree()
+                                                  ORDER BY id"""
+    products_value = ", ".join(
+                f"""({row[0]}, '{row[1].replace("'", "")}', {row[2]})""" for row in data
+            )
+    db_conn_manager.write_to_analytical_db(data, 'products', products_ddl, products_value)
+
+
+def syncronize_orders(db_conn_manager, data):
+    orders_ddl = """CREATE TABLE orders_temp (id UInt32, customer_id UInt32,
+                                              created_at DateTime, total_price Float32)
+                                              ENGINE = MergeTree() ORDER BY id """
+    orders_value = ", ".join(
+                f"""({row[0]}, {row[1]}, '{row[2].strftime('%Y-%m-%d %H:%M:%S')}',
+                     {row[3]})""" for row in data
+            )
+    db_conn_manager.write_to_analytical_db(data, 'orders', orders_ddl, orders_value)
+
+
+def syncronize_order_products(db_conn_manager, data):
+    order_products_ddl = """CREATE TABLE order_products_temp
+                            (order_id UInt32, product_id UInt32, quantity UInt32, 
+                             price Float32) ENGINE = MergeTree()
+                             ORDER BY (order_id, product_id)"""
+    order_products_value = ", ".join(
+                f"""({row[0]}, {row[1]}, {row[2]}, {row[3]})""" for row in data
+            )
+    db_conn_manager.write_to_analytical_db(data, 'order_products', order_products_ddl,
+                                           order_products_value)
+
 
 if __name__ == "__main__":
-    sync_data()
+    logging.basicConfig(
+                        level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        handlers=[
+                            logging.StreamHandler()
+                        ]
+    )
+    logger = logging.getLogger()
+    OLTP_DB_CONFIG = {
+        "dbname": "ecommerce",
+        "user": "user",
+        "password": "password",
+        "host": os.getenv('HOST_IP'),
+        "port": 5432,
+    }
+    OLAP_DB_CONFIG = {
+        "host": "localhost",
+        "port": 9001,
+        "database": "analytics_db",
+        "user": "user",
+        "password": "password",
+        "send_receive_timeout": 10
+    }
+    logging.info("Starting syncronization.")
+    db_conn_manager = DbManager(OLTP_DB_CONFIG, OLAP_DB_CONFIG)
+
+    customers = db_conn_manager.get_operational_data('customers')
+    syncronize_customers(db_conn_manager, customers)
+
+    products = db_conn_manager.get_operational_data('products')
+    syncronize_products(db_conn_manager, products)
+
+    orders = db_conn_manager.get_operational_data('orders')
+    syncronize_orders(db_conn_manager, orders)
+
+    order_products = db_conn_manager.get_operational_data('order_products')
+    syncronize_order_products(db_conn_manager, order_products)
+    logging.info("Synchronization completed.")
--- cloud_intro/end2end/analytical_layer/init.sql
+++ cloud_intro/end2end/analytical_layer/init.sql
@@ -1,30 +1,30 @@
 -- Create the customers table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS analytics_db.customers (
     id UInt32,
-    name String,
+    customer_name String,
     email String
 ) ENGINE = MergeTree()
 ORDER BY id;
 
 -- Create the products table
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS analytics_db.products (
     id UInt32,
-    name String,
+    product_name String,
     price Float32
 ) ENGINE = MergeTree()
 ORDER BY id;
 
 -- Create the orders table
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS analytics_db.orders (
     id UInt32,
     customer_id UInt32,
-    timestamp DateTime,
+    created_at DateTime,
     total_price Float32
 ) ENGINE = MergeTree()
 ORDER BY id;
 
 -- Create the order_products table
-CREATE TABLE IF NOT EXISTS order_products (
+CREATE TABLE IF NOT EXISTS analytics_db.order_products (
     order_id UInt32,
     product_id UInt32,
     quantity UInt32,
