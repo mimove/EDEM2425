3841b8e0a8082aecc9176bb79a183e66a969ff0e
fab1an12 <fabianmiulescu@gmail.com>
2025-03-05 19:04:13 +0100

    Update landing_flights

--- aws_setup/end2end/landed_flights.py
+++ aws_setup/end2end/landed_flights.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from initial_info import flights
 
 def get_landed_flights(flights):
-    current_time = datetime.now(timezone.utc).isoformat()  # Usando datetime con timezone UTC
+    current_time = datetime.now(timezone.utc).isoformat()
     
     landed_flights = [flight for flight in flights if flight["arrivalTime"] < current_time]
     
